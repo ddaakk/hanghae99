6cf01762dee560b53f9527b107641b71ba92b0a2
ksw <ksw@codeblock.co.kr>
2023-12-06 14:27:40 +0900

    feature: 커밋 전 ktlint 검증 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,9 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-import org.springframework.boot.gradle.tasks.bundling.BootJar
 
 plugins {
     id("org.springframework.boot") version "3.2.0"
     id("io.spring.dependency-management") version "1.1.4"
+    id("org.jlleitschuh.gradle.ktlint") version "12.0.2"
     kotlin("jvm") version "1.9.20"
     kotlin("plugin.spring") version "1.9.20"
 }
@@ -20,7 +20,6 @@ tasks {
         enabled = false
     }
 }
-
 
 configurations {
     compileOnly {
